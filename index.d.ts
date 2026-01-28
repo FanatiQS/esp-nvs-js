6f83b77922c10eefad66f5d48956e9b50fc5f9b6
FanatiQS <fanatiqs.com@gmail.com>
2026-01-28 05:47:22 +0100

    Add main entry

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import "./esptooljs";
+
+export * from "./nvs_parser.js";
+export { NVS } from "./nvs.js";
+export { nvs_transform_json, nvs_transform_html } from "./nvs_transform.js";
---
+++ index.js
@@ -0,0 +1,5 @@
+// @ts-check
+
+export * from "./nvs_parser.js";
+export { NVS } from "./nvs.js";
+export { nvs_transform_json, nvs_transform_html } from "./nvs_transform.js";
