22f0242bb1f2ee42e67e51e1c9dcd200d7165c68
FanatiQS <fanatiqs.com@gmail.com>
2026-02-02 13:35:05 +0100

    Add partitions table parse test

--- test/test.js
+++ test/test.js
@@ -4,28 +4,53 @@ import test from "node:test";
 import { createLoader } from "./loader.js";
 import { NVS } from "../src/nvs.js";
 import "./stub_serialport.js";
+import { firmware_generate_partitions } from "./firmware_generate.js";
 
-// Assert that all NVS pages are requested with correct address and size
-test("all pages requested", async () => {
-	// Creates empty firmware
-	const partition_addr = 0x9000;
-	const partition_size = 0x6000;
-	const firmware = new Uint8Array(partition_addr + partition_size);
-	firmware.fill(0xff);
-
-	// Creates load that asserts serial read requests
-	let addr_next = partition_addr;
-	let size_remaining = partition_size;
-	const loader = createLoader(firmware, async (addr, size) => {
-		assert.equal(addr, addr_next);
-		assert.equal(size, 0x1000);
-		addr_next += size;
-		size_remaining -= size;
+/**
+ * Creates a loader that asserts every page was requested
+ * @param {Uint8Array} firmware
+ * @param {{ addr: number, size: number }[]} partitions
+ */
+async function createLoaderAssertPartitions(firmware, partitions) {
+	let offset = 0;
+	return createLoader(firmware.buffer, (addr, size) => {
+		const partition = partitions[0];
+		assert(addr === (partition.addr + offset));
+		offset += size;
+		assert(offset <= partition.size);
+		if (offset === partition.size) {
+			partitions.shift();
+			offset = 0;
+		}
 	});
+}
 
-	// Runs test and asserts all pages were requested
-	const nvs = new NVS(loader);
-	nvs.addFlashAddr(addr_next, size_remaining);
+// Assert that all NVS pages in manually specified partition are requested
+test("all pages requested", async () => {
+	const addr = 0x9000;
+	const size = 0x4000;
+	const partitions = [ { addr, size } ];
+	const firmware = new Uint8Array(addr + size);
+	firmware.fill(0xff);
+	const nvs = new NVS(await createLoaderAssertPartitions(firmware, partitions));
+	nvs.addFlashAddr(addr, size);
 	await nvs.next();
-	assert.equal(size_remaining, 0);
+	assert(partitions.length === 0);
+});
+
+// Asserts that all existing NVS pages are requested with correct address and size
+test("pages from partition table", async () => {
+	const partitions = [
+		{ addr: 0x8000, size: 0xc00 },
+		{ addr: 0x9000, size: 0x2000 },
+		{ addr: 0x10000, size: 0x3000 }
+	];
+	const firmware = firmware_generate_partitions([
+		{ type: 0x0102, size: 0x2000, name: "foo" },
+		{ type: 0x0000, size: 0x5000, name: "bar" },
+		{ type: 0x0102, size: 0x3000, name: "baz" }
+	], 0x8000);
+	const nvs = new NVS(await createLoaderAssertPartitions(firmware, partitions));
+	await nvs.next();
+	assert(partitions.length === 0);
 });
