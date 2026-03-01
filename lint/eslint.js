fce6fd53632f7d03e3495c88e9418dfe098bb6cb
FanatiQS <fanatiqs.com@gmail.com>
2026-03-02 00:47:56 +0100

    Refactor eslint config

--- lint/eslint.js
+++ lint/eslint.js
@@ -4,38 +4,8 @@ import { defineConfig } from "eslint/config";
 
 // cspell: ignore camelcase no-plusplus
 
-const rules_base = {
-	"one-var": [ "error", "never" ],
-	"func-style": [ "error", "declaration" ],
-	"object-shorthand": [ "error", "methods" ],
-	"id-length": [ "error", { min: 3, exceptions: [ "i", "j", "ns", "ui" ] } ],
-	"capitalized-comments": [ "error", "always", { ignoreInlineComments: true, ignorePattern: "cspell: .*" } ],
-	"eqeqeq": [ "error", "always", { null: "ignore" } ],
-	"complexity": [ "error", { variant: "modified" } ],
-	"curly": [ "error", "multi-line" ],
-	"prefer-const": [ "error", { destructuring: "all" } ],
-	"camelcase": "off",
-	"no-plusplus": "off",
-	"no-bitwise": "off",
-	"no-ternary": "off",
-	"no-continue": "off",
-	"no-eq-null": "off",
-	"max-statements": "off",
-	"max-lines-per-function": "off",
-	"max-lines": "off",
-	"max-params": "off",
-	"no-await-in-loop": "off",
-	"no-inline-comments": "off",
-	"init-declarations": "off",
-	"no-multi-assign": "off",
-	"no-magic-numbers": "off",
-	"sort-keys": "off",
-	"sort-imports": "off",
-	"no-undefined": "off"
-};
-
 // Test server files that run in node
-const files_server = [
+const files_server_test = [
 	"test/index.js",
 	"test/partitions.js"
 ];
@@ -45,35 +15,67 @@ export default defineConfig([
 		ignores: [ "coverage/**/*" ]
 	},
 	{
-		files: [ "src/**/*.js" ],
-		plugins: { js },
-		extends: [ "js/all" ],
+		name: "default",
+		files: [ "src/**/*.js", "test/**/*.js" ],
+		rules: {
+			...js.configs.all.rules,
+			"one-var": [ "error", "never" ],
+			"func-style": [ "error", "declaration" ],
+			"object-shorthand": [ "error", "methods" ],
+			"id-length": [ "error", { min: 3, exceptions: [ "i", "j", "ns", "ui" ] } ],
+			"capitalized-comments": [ "error", "always", { ignoreInlineComments: true, ignorePattern: "cspell: .*" } ],
+			"eqeqeq": [ "error", "always", { null: "ignore" } ],
+			"complexity": [ "error", { variant: "modified" } ],
+			"curly": [ "error", "multi-line" ],
+			"prefer-const": [ "error", { destructuring: "all" } ],
+			"camelcase": "off",
+			"no-plusplus": "off",
+			"no-bitwise": "off",
+			"no-ternary": "off",
+			"no-continue": "off",
+			"no-eq-null": "off",
+			"max-statements": "off",
+			"max-lines-per-function": "off",
+			"max-lines": "off",
+			"max-params": "off",
+			"no-await-in-loop": "off",
+			"no-inline-comments": "off",
+			"init-declarations": "off",
+			"no-multi-assign": "off",
+			"no-magic-numbers": "off",
+			"sort-keys": "off",
+			"sort-imports": "off",
+			"no-undefined": "off"
+		}
+	},
+	{
+		name: "browser",
+		ignores: files_server_test,
 		languageOptions: {
 			globals: {
 				SerialPort: true,
 				...globals.browser
 			}
-		},
+		}
+	},
+	{
+		name: "src exceptions",
+		files: [ "src/**/*.js" ],
 		rules: {
-			...rules_base,
 			"prefer-destructuring": "off",
 			"prefer-rest-params": "off"
 		}
 	},
 	{
+		name: "test exceptions",
 		files: [ "test/**/*.js" ],
-		ignores: files_server,
-		plugins: { js },
-		extends: [ "js/all" ],
+		ignores: files_server_test,
 		languageOptions: {
 			globals: {
-				SerialPort: true,
 				test: true,
-				...globals.browser
 			}
 		},
 		rules: {
-			...rules_base,
 			"no-new": "off",
 			"no-empty-function": "off",
 			"class-methods-use-this": "off",
@@ -82,17 +84,14 @@ export default defineConfig([
 		}
 	},
 	{
-		files: [
-			"test/index.js",
-			"test/partitions.js"
-		],
-		plugins: { js },
-		extends: [ "js/all" ],
+		name: "test server",
+		files: files_server_test,
 		languageOptions: {
-			globals: { ...globals.nodeBuiltin }
+			globals: {
+				...globals.nodeBuiltin
+			}
 		},
 		rules: {
-			...rules_base,
 			"capitalized-comments": "off"
 		}
 	}
